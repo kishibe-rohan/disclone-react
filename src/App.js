2f722974d3c96fffda3c3ec6bd4d5ed0fc3d99b0
kishibe-rohan <devrohan2000@gmail.com>
2022-03-15 11:55:39 +0530

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./components/Login";
+import Home from "./components/Home";
+
 function App() {
-  return <div className="text-3xl font-bold underline">Disclone</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/channels" element={<Home />} />
+        <Route exact path="/channels/:id" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import Header from './Header'
+import Hero from './Hero'
+
+function Login(){
+   return(
+       <>
+       <Header/>
+       <Hero/>
+       </>
+   )
+}
+
+export default Login;
